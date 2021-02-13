0675efc95930e2ddd0bcfe4b29163412ddb0d2bf
IgorCarvalho14 <igorcarvalhodasilva012@gmail.com>
2021-02-13 18:40:29 -0400

    Add fixed errors and Add examples

--- Sort/Heap/C/heap.c
+++ Sort/Heap/C/heap.c
@@ -4,21 +4,21 @@ void criaHeap(int vet[], int inicio, int final){
 
     int aux = vet[inicio];
     int j = inicio*2+1;
-    while(j < final){
+    while(j <= final){
         if(j < final){
             if(vet[j] < vet[j+1]){
                 j++;
             }
         }
         if(aux < vet[j]){
-            vet[i] = vet[j];
+            vet[inicio] = vet[j];
             inicio = j;
             j = 2*inicio+1;
         }else{
             j = final+1;
         }
     }
-    vet[i] = aux;
+    vet[inicio] = aux;
 }
 
 void heapSort(int vet[], int tam){
@@ -35,4 +35,20 @@ void heapSort(int vet[], int tam){
         vet[i] = aux;
         criaHeap(vet, 0, i-1);
     }
+}
+
+
+void main(){
+
+    //Exemplo
+    int i, tam = 9;
+    int vet[] = {5,7,9,2,8,4,3,6,1};
+
+    heapSort(vet,tam);
+
+    //Imprimindo o vetor
+    for(i = 0; i < tam; i++){
+        printf("%d ", vet[i]);
+    }
+    printf("\n");
 }
